beb50766b616d5d6665483155c886ddbe358895e
yutianfei1127 <yutianfei1127@163.com>
2016-11-16 09:40:56 +0800

    添加、修改图片

--- app/src/main/java/com/wsy/plan/map/fragment/CashMapFragment.java
+++ app/src/main/java/com/wsy/plan/map/fragment/CashMapFragment.java
@@ -23,6 +23,7 @@ public class CashMapFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map_cash, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_map_cash, container, false);
+        return rootView;
     }
 }
